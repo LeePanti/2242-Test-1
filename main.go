dea8c1d66f39d9387c5f12d4495f07746b05c3eb
Lee Panti <2019119651@ub.edu.bz>
2023-03-31 07:01:34 -0600

    final comment edits

--- main.go
+++ main.go
@@ -8,16 +8,23 @@ import (
 	"time"
 )
 
+// home page handler function
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// creates a new template for the home page
 	html := template.Must(template.ParseFiles("static/home.html"))
 
+	// data to be added to the template
 	bio := "My Name is Lee Edbert Panti. I am 20 years old currently enrolled in the AINT program at the University of Belize. I love learning new things and figuring out solutions to programming related problems. This is my first Website using Golang programming language."
 	html.Execute(w, bio)
 }
 
+// greeting handler function
 func greeting(w http.ResponseWriter, r *http.Request) {
+	// template for the greeting page
 	html := template.Must(template.ParseFiles("static/greeting.html"))
 
+	// find the time and weekday to be displayed
 	dateTime := make(map[string]string)
 	dateTime["time"] = time.Now().Format("15:04")
 	dateTime["weekday"] = time.Now().Weekday().String()
@@ -25,9 +32,12 @@ func greeting(w http.ResponseWriter, r *http.Request) {
 	html.Execute(w, dateTime)
 }
 
+// random handler function
 func random(w http.ResponseWriter, r *http.Request) {
+	// template for the random page
 	html := template.Must(template.ParseFiles("static/random.html"))
 
+	// quotes to be added to the template
 	quotes := [6]string{
 		"Physics is the universe's operating system.",
 		"A computer once beat me at chess, but it was no match for me at kick boxing.",
@@ -37,6 +47,7 @@ func random(w http.ResponseWriter, r *http.Request) {
 		"Before software should be reusable, it should be usable.",
 	}
 
+	// passing in only one randum quote
 	randQuote := rand.Intn(len(quotes))
 
 	html.Execute(w, quotes[randQuote])
@@ -44,13 +55,17 @@ func random(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// create a new mux router
 	mux := http.NewServeMux()
+	// supply the end points and each handler function
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/greeting", greeting)
 	mux.HandleFunc("/random", random)
 
+	// supply the styles folder to let the templates use the stylesheet
 	mux.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("./styles"))))
 
+	// create a new server
 	log.Println("Starting server on port 4000...")
 	err := http.ListenAndServe(":4000", mux)
 	if err != nil {
